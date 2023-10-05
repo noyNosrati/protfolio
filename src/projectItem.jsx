b7e2e70531b4c1e211d774c045ed4c205793737e
noyNosrati <noynosrati@gmail.com>
2023-10-05 12:56:37 +0300

    skills done

--- src/projectItem.jsx
+++ src/projectItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ProjectItem({ img, title, text, skills, gitLink, pLink }) {
     return (
-        <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-white  to-[#1e0d3a]'>
+        <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-white  to-[#2813a0]'>
             <img src={img} alt="title" className='rounded-xl group-hover:opacity-5 w-full h-full object-left object-center' />
             <div className='absolute justify-center items-center opacity-0 group-hover:opacity-100'>
                 <h3 className='py-1 text-2xl font-bold text-white tracking-wide text-center w-full'>

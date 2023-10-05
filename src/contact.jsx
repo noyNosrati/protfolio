0ce18e1ff519fa2f586566479b00bb29629c02da
noyNosrati <noynosrati@gmail.com>
2023-10-05 18:13:28 +0300

    done styling

--- src/contact.jsx
+++ src/contact.jsx
@@ -16,8 +16,7 @@ export const Contact = () => {
     };
 
     return (
-        <div id='contact' className='p-4 lg:p-20 flex flex-col items-center justify-center'>
-            {/* <h1 className="text-[52px] font-semibold mb-20 leading-normal uppercase text-[#341d5c]">Contact Me</h1> */}
+        <div id='contact' className='p-4 pt-10 lg:p-20 flex flex-col items-center justify-center'>
             <h2 className='text-4xl font-bold text-center text-[#001b5e] mb-10'>Contact Me</h2>
 
             <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-2 lg:w-1/2">
@@ -25,7 +24,7 @@ export const Contact = () => {
                     <input name="user_name" className="w-full lg:my-3 my-4 rounded-lg bg-[#cfcff0] p-4  b_glow text-xl text-slate-600" placeholder="Enter Your Name" type="text" />
                     <input name="user_email" className="w-full lg:my-3 my-4 rounded-lg bg-[#cfcff0] p-4  b_glow text-xl text-slate-600" placeholder="Enter Your Email" type="text" />
                 </div>
-                <textarea className="w-full my-3 rounded-lg bg-[#cfcff0] p-4  b_glow text-xl text-slate-600" placeholder="Write Your Massage..." name="message" cols="20" rows="7"></textarea>
+                <textarea className="w-full my-3 rounded-lg bg-[#cfcff0] p-4  b_glow text-xl text-slate-600" placeholder="Write Your Massage..." name="message" cols="20" rows="6"></textarea>
                 {/* <input type="submit" value="Send" className='border' /> */}
                 <button className="neno-button shadow-xl hover:shadow-indigo-400 text-white border-2 border-indigo-950 bg-indigo-950 hover:text-indigo-100 hover:bg-[#845ac9] rounded-lg py-4 px-8 my-6 uppercase  overflow-hidden b_glow text-xl" type="submit">Submit</button>
             </form>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// Import AOS styles
+import 'aos/dist/aos.css';
+
+// Import AOS library
+import AOS from 'aos';
+
+// Initialize AOS
+AOS.init();
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
